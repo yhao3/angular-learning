0d36d9465772abf9d9fb3319b87d0e68d211ebf8
yhao3 <kyrieeeee2@gmail.com>
2023-05-22 19:33:55 +0800

    add first angular app

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+}
