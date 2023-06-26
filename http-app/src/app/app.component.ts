1ab1bf10a239d7b12b9deab3ec2c259b7e448ec4
yhao3 <kyrieeeee2@gmail.com>
2023-06-26 22:32:48 +0800

    Revert "Using a Service for Http Requests"
    This reverts commit 999011d0c6b18d25c244c011f780d3aaf1c453b4.

--- http-app/src/app/app.component.ts
+++ http-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { Post } from './post.model';
-import { PostsService } from './posts.service';
 
 
 @Component({
@@ -13,35 +13,52 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient,
-              private postService: PostsService) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.postService
-      .fetchPosts()
-      .subscribe(posts => {
-        this.isFetching = false;
-        this.loadedPosts = posts;
-      });
+    this.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
-    this.postService.createAndStorePost(postData.title, postData.content);
+    this.http.post<{ [key: string]: Post }>(
+      'https://ng-complete-guide-1b8d7-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
+      postData
+    )
+    .subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
     // Send Http request
-    this.postService
-      .fetchPosts()
-      .subscribe(posts => {
-        this.isFetching = false;
-        this.loadedPosts = posts;
-      });
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  fetchPosts() {
+    this.isFetching = true;
+    this.http
+      .get<{ [key: string]: Post }>('https://ng-complete-guide-1b8d7-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+      .pipe(
+        map(responseData => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe(posts => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      }
+    );
   }
 
 }
